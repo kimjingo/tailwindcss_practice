d0e1812eadf685cc978f3976779069c7c445a1d1
Jingoo Kim <kimjingo@gmail.com>
2022-04-08 22:03:33 -0700

    setup tailwind css

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container mx-auto px-4">
+
+        <h1 class="text-3xl font-bold underline">
+            Hello world!!!
+        </h1>
+    </div>
+</body>
+</html>
